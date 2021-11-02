b5010db1caa44ade2eed7b441fc032e83c2a7b5d
Alexanre Awadallak <alexandre.awadallak@gmail.com>
2021-11-01 23:53:52 -0300

    feat: build tags implemented

---
+++ config/logger_dev.go
@@ -0,0 +1,19 @@
+//go:build dev
+
+package config
+
+import (
+	"go.uber.org/zap"
+)
+
+var sugarLogger *zap.SugaredLogger
+
+func InitLogger() {
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync()
+	sugarLogger = logger.Sugar()
+}
+
+func Logger() *zap.SugaredLogger {
+	return sugarLogger
+}
