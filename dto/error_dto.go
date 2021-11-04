83da9ec3ca85ff4b15e98b9a40761a5d18d2d31a
Alexanre Awadallak <alexandre.awadallak@gmail.com>
2021-11-04 00:26:58 -0300

    feat: entity to dto

--- dto/error_dto.go
+++ dto/error_dto.go
@@ -1,6 +1,17 @@
 package dto
 
+import err "latest/domain/error"
+
 type ErrorDTO struct {
-	Code    int
-	Message string
+	Code    int    `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+	Module  string `json:"module,omitempty"`
+}
+
+func (e *ErrorDTO) EntityToDTO(err *err.Err) ErrorDTO {
+	return ErrorDTO{
+		Code:    err.Code(),
+		Message: err.Message(),
+		Module:  err.Module(),
+	}
 }
